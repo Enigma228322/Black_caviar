cb692303d42f244ee5b4cd0de5f35f4ebbf7d809
Vildan <vildan.safin.99@gmail.com>
2019-08-31 20:30:24 +0500

    Added dropdown menu

---
+++ js/script.js
@@ -0,0 +1,29 @@
+//    var menu = document.getElementByClassName('main-menu');
+//    for(var i = 0; i < menu.length; i++)
+//    {
+//        menu[i].addEventListener('mouseenter', showMenu,false);
+//        menu[i].addEventListener('mouseleave', hideMenu,false);
+//    }
+//
+//    function showMenu()
+//    {
+//        if(this.children.length > 1)
+//        {
+//            this.children[1].style.height = 'auto';
+//            this.children[1].style.opacity = '1';
+//            this.children[1].style.overflow = 'visible';
+//        }
+//        else false;
+//    }
+//
+//    function hideMenu()
+//    {
+//        if(this.children.length > 1)
+//        {
+//            this.children[1].style.height = '0';
+//            this.children[1].style.opacity = '0';
+//            this.children[1].style.overflow = 'hidden';
+//        }
+//        else false;
+//    }
+//
