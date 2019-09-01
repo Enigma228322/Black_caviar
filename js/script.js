8bd6704432c84383016c7af168a6894b79fab33b
Vildan <vildan.safin.99@gmail.com>
2019-09-02 00:17:16 +0500

    Added new moving menu v2

--- js/script.js
+++ js/script.js
@@ -1,29 +1,28 @@
-//    var menu = document.getElementByClassName('main-menu');
-//    for(var i = 0; i < menu.length; i++)
-//    {
-//        menu[i].addEventListener('mouseenter', showMenu,false);
-//        menu[i].addEventListener('mouseleave', hideMenu,false);
-//    }
-//
-//    function showMenu()
-//    {
-//        if(this.children.length > 1)
-//        {
-//            this.children[1].style.height = 'auto';
-//            this.children[1].style.opacity = '1';
-//            this.children[1].style.overflow = 'visible';
-//        }
-//        else false;
-//    }
-//
-//    function hideMenu()
-//    {
-//        if(this.children.length > 1)
-//        {
-//            this.children[1].style.height = '0';
-//            this.children[1].style.opacity = '0';
-//            this.children[1].style.overflow = 'hidden';
-//        }
-//        else false;
-//    }
-//
+    var show_main_menu = false;
+
+    function swipeMenu() {
+        var menu = document.getElementById('main-menu-1');
+        if(show_main_menu)
+        {
+            menu.style.setProperty('width','0px');
+            menu.style.setProperty('transition','all 0.3s ease-in');
+            for(var i = 1; i <= 3;i++)
+            {
+                document.getElementById('menu-item-'+i).style.setProperty('opacity', '0');
+                document.getElementById('menu-item-'+i).style.setProperty('transition','opacity 0.2s ease-in')
+            }
+            show_main_menu = false;
+        }
+        else
+        {
+            menu.style.setProperty('width','200px');
+            menu.style.setProperty('transition','all 0.3s ease-in');
+            for(var i = 1; i <= 3;i++)
+            {
+                document.getElementById('menu-item-'+i).style.setProperty('opacity', '1');
+                document.getElementById('menu-item-'+i).style.setProperty('transition','opacity 0.5s ease-in')
+            }
+            show_main_menu = true;
+        }
+    }
+
